8d3a49f3fd3e2e288dff9ee6506e3f217f9285fb
wuzhxyl <351563763@qq.com>
2018-01-04 13:37:03 +0800

    Update MainActivity.java

--- custom/app/src/main/java/com/ilifesmart/custom/MainActivity.java
+++ custom/app/src/main/java/com/ilifesmart/custom/MainActivity.java
@@ -8,20 +8,10 @@ import com.ilifesmart.widgets.CircleTextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CircleTextView mCircleTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mCircleTextView = (CircleTextView) findViewById(R.id.circleTextView);
     }
 
-    public void onViewClick(View view) {
-        switch (view.getId()) {
-            case R.id.circleTextView:
-                int number = (int) Math.floor(Math.random()*1000) + 1000;
-                mCircleTextView.setTitle(Integer.valueOf(number).toString());
-        }
-    }
 }
